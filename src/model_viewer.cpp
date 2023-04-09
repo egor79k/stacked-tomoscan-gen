c7d9f420753f5454828da27c0fa302519b8dfbe8
Egor Kiselev <kiselev.el@phystech.edu>
2023-04-09 19:05:31 +0300

    Model viewer updated, wheel models added

--- src/model_viewer.cpp
+++ src/model_viewer.cpp
@@ -4,23 +4,23 @@
 #include "TomoP3DModel.hpp"
 
 
-void show_demo_animation(int size, char* models_file, int model_id);
+void show_demo_animation(int size, int view_size, int view_angle, char* models_file, int model_id);
 
 
 int main(int argc, char** argv) {
-    if (argc != 4)
+    if (argc != 6)
     {
-        printf("Usage: ./strg <size> <models_lib> <model_id>\n");
+        printf("Usage: ./model_viewer <size> <view_size> <angle> <models_lib> <model_id>\n");
         return 1;
     }
 
-    show_demo_animation(atoi(argv[1]), argv[2], atoi(argv[3]));
+    show_demo_animation(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), argv[4], atoi(argv[5]));
 
     return 0;
 }
 
 
-void show_demo_animation(int size, char* models_file, int model_id) {
+void show_demo_animation(int size, int view_size, int view_angle, char* models_file, int model_id) {
     int h_det = size;
     int v_det = size;
     int angles_num = 360;
@@ -35,6 +35,7 @@ void show_demo_animation(int size, char* models_file, int model_id) {
     cv::Mat_<float> projections(3, proj_mat_shape);
 
     TomoP3DModel model(models_file, model_id);
+    model.rotate(view_angle, 0, 0, 0, 0, 0);
     model.sinogram(reinterpret_cast<float*>(projections.data), h_det, v_det, 0, size, size, angles);
     cv::normalize(projections, projections, 0, 1, cv::NORM_MINMAX);
 
@@ -45,6 +46,7 @@ void show_demo_animation(int size, char* models_file, int model_id) {
 
     while (key != 'q') {
         cv::Mat_<float> slice(v_det, h_det, reinterpret_cast<float*>(projections.data) + h_det * v_det * angle);
+        cv::resize(slice, slice, {view_size, view_size});
         cv::imshow("Display Image", slice);
         key = cv::waitKeyEx(20);
 
