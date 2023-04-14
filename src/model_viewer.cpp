d573af8f0cc0e1bb45b487ad8a7e7d912721d74f
Egor Kiselev <kiselev.el@phystech.edu>
2023-04-14 11:33:59 +0300

    Model viewer updated: video saving added

--- src/model_viewer.cpp
+++ src/model_viewer.cpp
@@ -4,23 +4,26 @@
 #include "TomoP3DModel.hpp"
 
 
-void show_demo_animation(int size, int view_size, int view_angle, char* models_file, int model_id);
+void show_demo_animation(int size, int view_size, int view_angle, char* models_file, int model_id, char* output_file=nullptr);
 
 
 int main(int argc, char** argv) {
-    if (argc != 6)
-    {
-        printf("Usage: ./model_viewer <size> <view_size> <angle> <models_lib> <model_id>\n");
+    if (argc == 6) {
+        show_demo_animation(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), argv[4], atoi(argv[5]));
+    }
+    else if (argc == 7) {
+        show_demo_animation(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), argv[4], atoi(argv[5]), argv[6]);
+    }
+    else {
+        printf("Usage: ./model_viewer <size> <view_size> <angle> <models_lib> <model_id> [output_video_file]\n");
         return 1;
     }
-
-    show_demo_animation(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), argv[4], atoi(argv[5]));
 
     return 0;
 }
 
 
-void show_demo_animation(int size, int view_size, int view_angle, char* models_file, int model_id) {
+void show_demo_animation(int size, int view_size, int view_angle, char* models_file, int model_id, char* output_file) {
     int h_det = size;
     int v_det = size;
     int angles_num = 360;
@@ -42,6 +45,12 @@ void show_demo_animation(int size, int view_size, int view_angle, char* models_f
     int angle = 0;
     int key = 0;
 
+    cv::VideoWriter video_writer;
+
+    if (output_file != nullptr) {
+        video_writer.open(output_file, cv::VideoWriter::fourcc('M','P','4','V'), 24, {view_size, view_size});
+    }
+
     cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
 
     while (key != 'q') {
@@ -50,9 +59,21 @@ void show_demo_animation(int size, int view_size, int view_angle, char* models_f
         cv::imshow("Display Image", slice);
         key = cv::waitKeyEx(20);
 
+        if (output_file != nullptr) {
+            cv::Mat rgb_slice;
+            cv::normalize(slice, slice, 0, 255, cv::NORM_MINMAX);
+            slice.convertTo(rgb_slice, CV_8U);
+            cv::cvtColor(rgb_slice, rgb_slice, cv::COLOR_GRAY2BGR);
+            video_writer.write(rgb_slice);
+        }
+
         ++angle;
         if (angle == angles_num) {
             angle = 0;
         }
+    }
+
+    if (output_file != nullptr) {
+        video_writer.release();
     }
 }
